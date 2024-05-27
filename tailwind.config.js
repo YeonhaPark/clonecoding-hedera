a3ea9a0c2e647c9fc65f42dbce6d0336f32562f0
YeonhaPark <yonaprisca@gmail.com>
2024-05-27 17:19:48 +0900

    feat: extend tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          600: "#3EC878",
+          700: "#21A056",
+        },
+        gray: {
+          100: "#EAEAEA",
+          800: "#464646",
+          900: " #222",
+        },
+      },
+    },
   },
   plugins: [],
 };
