2f69976d73c864d2a8d86f1fccf42c123b9ca81b
YeonhaPark <yonaprisca@gmail.com>
2024-05-28 09:06:43 +0900

    fix: change tag on Header div -> header

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -2,7 +2,7 @@ import Logo from "../atoms/Logo";
 import Nav from "../molecules/Nav";
 export default function Header() {
   return (
-    <div className="fixed flex items-center">
+    <header className="fixed flex items-center z-10">
       <Logo />
       <Nav />
       <button className="header-quickstart btn-white">QuickStart</button>
@@ -11,6 +11,6 @@ export default function Header() {
           <input type="search" className="search" placeholder="search" />
         </div>
       </form>
-    </div>
+    </header>
   );
 }
